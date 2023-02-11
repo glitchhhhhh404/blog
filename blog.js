9eb957dc0f67df843228afd1caa5af5d10c50c7d
glitch404 <124876651+glitchhhhhh404@users.noreply.github.com>
2023-02-10 21:37:54 -0300

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+
+    <title>Meu Blog</title>
+
+  </head>
+
+  <body>
+
+    <header>
+
+      <h1>Meu Blog</h1>
+
+    </header>
+
+    <nav>
+
+      <ul>
+
+        <li><a href="#post1">Post 1</a></li>
+
+        <li><a href="#post2">Post 2</a></li>
+
+        <li><a href="#post3">Post 3</a></li>
+
+      </ul>
+
+    </nav>
+
+    <main>
+
+      <section id="post1">
+
+        <h2>Post 1</h2>
+
+        <p>Conteúdo do post 1</p>
+
+      </section>
+
+      <section id="post2">
+
+        <h2>Post 2</h2>
+
+        <p>Conteúdo do post 2</p>
+
+      </section>
+
+      <section id="post3">
+
+        <h2>Post 3</h2>
+
+        <p>Conteúdo do post 3</p>
+
+      </section>
+
+    </main>
+
+    <footer>
+
+      <p>Copyright &copy; 2023 Meu Blog</p>
+
+    </footer>
+
+  </body>
+
+</html>
+
